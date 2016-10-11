1cadd9035fc69bc4a193151470e121a469b92636
Brian Zhou <bzhou4@gmail.com>
2016-10-11 13:35:50 -0700

    (add) uploaded a blog-creation page

--- client/app/blog-component/blog-creator/blog-creator.jsx
+++ client/app/blog-component/blog-creator/blog-creator.jsx
@@ -1,1 +1,17 @@
 // blog-creator will be used to create new blog-pages and add entries into blog
+// THIS CAN BE AN ADMIN-ONLY PAGE SO UI ISN'T IMPORTANT
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class BlogCreator extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+
+	// need db to be hooked
+
+}
