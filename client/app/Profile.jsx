ecea9afc0abe1fb542807fc91e4fd225acf7e609
Brian Zhou <bzhou4@gmail.com>
2016-10-13 13:57:09 -0700

    (add) added a profile.jsx page

---
+++ client/app/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Profile extends React.Component {
+
+	constructor(props) {
+		super(props)
+
+	}
+
+	// a pull to the db for that specific user
+
+
+
+}
+
+export default Profile;
